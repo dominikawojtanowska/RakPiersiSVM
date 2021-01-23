03a67bdecb8018c29df1c9a514505de7eb1ad2e4
Dominika <Dominika@DESKTOP-4VPBBJI>
2021-01-23 21:02:35 +0100

    dokladnoosc modelu trzeba bedzie poprawic

--- RakPiersi/RakPiersi.py
+++ RakPiersi/RakPiersi.py
@@ -4,22 +4,16 @@ import sklearn as skl
 import pandas as pandas
 import matplotlib as plt
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
-
-#splitowanie danych 
-def split_data(data, scale):
-    shuff = np.random.permutation(len(data))
-    size = int(len(data) * scale)
-    test = shuff[:size]
-    train = shuff[size:]
-    return data.iloc[train], data.iloc[test]
 
 #wczytsanie naszych danych
 data = pandas.read_table("wdbc.data", sep=",", header=None)
 
 
 #podział danych według naszej funkcji
-train, test = split_data(data, 0.25)
+train, test = train_test_split(data, test_size = 0.2, random_state = 1)
 #train
 #x = train[:, (2,3)]
 #y = (train[1] == 'M').astype(np.float64)
@@ -29,7 +23,18 @@ print(train)
 x = train.iloc[:, 2:4].values
 y = train.iloc[:, 1].values
 
+X_test = test.iloc[:, 2:4].values
+Y_test = test.iloc[:, 1].values
+
 classifier = SVC(kernel='rbf', random_state = 1)
 classifier.fit(x,y)
+
+Y_pred = classifier.predict(X_test)
+test["Predictions"] = Y_pred
+
+
+cm = confusion_matrix(Y_test,Y_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
 
 #print("data", len(train), "dataa", len(test))
