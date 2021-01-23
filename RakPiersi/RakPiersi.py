002944af5acca1cfe157177012f10c329848b359
Dominika <Dominika@DESKTOP-4VPBBJI>
2021-01-23 20:39:12 +0100

    svm

--- RakPiersi/RakPiersi.py
+++ RakPiersi/RakPiersi.py
@@ -2,6 +2,8 @@
 import numpy as np
 import sklearn as skl
 import pandas as pandas
+import matplotlib as plt
+from sklearn.svm import SVC
 
 
 #splitowanie danych 
@@ -14,9 +16,20 @@ def split_data(data, scale):
 
 #wczytsanie naszych danych
 data = pandas.read_table("wdbc.data", sep=",", header=None)
-#data.info();
+
 
 #podział danych według naszej funkcji
 train, test = split_data(data, 0.25)
+#train
+#x = train[:, (2,3)]
+#y = (train[1] == 'M').astype(np.float64)
+
+print(train)
+
+x = train.iloc[:, 2:4].values
+y = train.iloc[:, 1].values
+
+classifier = SVC(kernel='rbf', random_state = 1)
+classifier.fit(x,y)
 
 #print("data", len(train), "dataa", len(test))
