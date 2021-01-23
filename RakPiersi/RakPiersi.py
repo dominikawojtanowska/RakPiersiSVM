a5924b4daf5eab5cb31a6b69aad3511c347e0a36
Dominika <Dominika@DESKTOP-4VPBBJI>
2021-01-23 21:32:16 +0100

    poprawki

--- RakPiersi/RakPiersi.py
+++ RakPiersi/RakPiersi.py
@@ -2,10 +2,12 @@
 import numpy as np
 import sklearn as skl
 import pandas as pandas
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder
 
 
 #wczytsanie naszych danych
@@ -32,9 +34,25 @@ classifier.fit(x,y)
 Y_pred = classifier.predict(X_test)
 test["Predictions"] = Y_pred
 
+print(test)
 
-cm = confusion_matrix(Y_test,Y_pred)
-accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
+#cm = confusion_matrix(Y_test,Y_pred)
+#accuracy = float(cm.diagonal().sum())/len(Y_test)
+#print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
+
+
+plt.figure(figsize = (7,7))
+X_set, y_set = x, y
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('black', 'white')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'orange'))(i), label = j)
+
+plt.title('rak')
+
+plt.show()
 
 #print("data", len(train), "dataa", len(test))
