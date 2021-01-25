3df402d1a19d0aaf30c1688c56b159c7eeec00dc
Piotr Debowski <depiotr01@gmail.com>
2021-01-25 14:02:23 +0100

    code clean

--- RakPiersi/RakPiersi.py
+++ RakPiersi/RakPiersi.py
@@ -40,10 +40,8 @@ def plot_svc_decision_function(clf, ax=None):
 data = pandas.read_table("wdbc.data", sep=",", header=None)
 data.drop([data.columns[13], data.columns[10], data.columns[11], data.columns[16], data.columns[31]], axis=1).head(2)
 
-
 y = data.iloc[:, 1].values
 train, test = train_test_split(data, test_size = 0.25, random_state = 1, stratify=y)
-
 
 x = train.iloc[:, 2:].values 
 y = train.iloc[:, 1].values
@@ -53,48 +51,30 @@ Y_test = test.iloc[:, 1].values
 
 normalizer = Normalizer()
 normalizer.transform(x)
-
+normalizer.transform(X_test)
 
 classifier = SVC(kernel='linear', random_state = 1, C=10)
 classifier.fit(x, y)
-
-normalizer = Normalizer()
-normalizer.transform(X_test)
-
 Y_pred = classifier.predict(X_test)
-test["Predictions"] = Y_pred
-
-
 cm = confusion_matrix(Y_test,Y_pred)
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
-
-
+print("\nAccuracy Of SVM with C=10 For The Given Dataset : ", accuracy)
 
 classifierPoly = Pipeline([
     ("scaler", StandardScaler()), ("svm,clf",SVC(kernel="poly", C=13))])
 classifierPoly.fit(x, y)
-
 Y_pred = classifierPoly.predict(X_test)
-test["Predictions"] = Y_pred
-
 cm = confusion_matrix(Y_test,Y_pred)
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of SVM For The Given Dataset9999999 : ", accuracy)
-
-
+print("\nAccuracy Of SVM For The Given Dataset karnel=poly: ", accuracy)
 
 classifier2 = Pipeline([
     ("scaler", StandardScaler()), ("linear_svc", LinearSVC(C=5, loss="hinge"))])
 classifier2.fit(x, y)
-
 Y_pred = classifier2.predict(X_test)
-test["Predictions"] = Y_pred
-
 cm = confusion_matrix(Y_test,Y_pred)
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of SVM For The Given Dataset22222 : ", accuracy)
-
+print("\nAccuracy Of LinearSVC(C=5, loss=hinge): ", accuracy)
 
 targets = np.copy(y)
 for i in range(targets.size):
@@ -102,6 +82,5 @@ for i in range(targets.size):
         targets[i]=1
     else:
         targets[i]=2
-
 
 plt.scatter(x[:, 17], x[:,24], c=targets, s=50, cmap='spring')
